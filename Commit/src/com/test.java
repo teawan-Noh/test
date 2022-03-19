55c4673758b35965ecd205d1ba10c2d66f0710e2
DESKTOP-QA0M42J\taewan <ntw2100@gmail.com>
2022-03-19 20:03:28 +0900

    구디 임미영쌤 뺵업

--- Commit/src/com/test.java
+++ Commit/src/com/test.java
@@ -3,8 +3,6 @@ package com;
 public class test {
 	private void sysout() {
 		System.out.println("commit");
-		System.out.println("commit22");
-		System.out.println("commit2c");
 		
 
 	}
---
+++ JavaRemind/src/java/com/Math.java
@@ -0,0 +1,15 @@
+package java.com;
+
+public class Math {
+	
+	static int sum(int a, int b) {
+		int result = a + b;
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		
+		sum(1,2);
+	}
+}
